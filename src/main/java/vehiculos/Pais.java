da5aae8b6839d73c25bbbfc6814f6a3bf8739510
santiagoperez0 <jperezpi@unal.edu.co>
2022-05-10 20:58:15 -0500

    sssss

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,7 +1,36 @@
-package vehiculos;
-
 public class Pais {
+	private String nombre;
+	static Map<Pais, Integer> ventasPorPais = new HashMap <Pais, Integer>();
 
+	public static Pais paisMasVendedor()
+	{
+		int valorMax = -1;
+		Pais paisMasVendedor = null;
+
+		for (Entry<Pais, Integer> entry : ventasPorPais.entrySet()) {
+		    final int valorActual = entry.getValue();
+
+		    if (valorActual > valorMax)
+		    {
+		    	valorMax = valorActual;
+		    	paisMasVendedor = entry.getKey();
+		    }
+		}
+
+		return paisMasVendedor;
+	}
+
+	public Pais(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 
 }
